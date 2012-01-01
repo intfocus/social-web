bc0570871904ff601f9a0530e922ff907ad3c975
Lihao Albert <info@intfocus.com>
2012-01-01 15:27:45 +0800

    first commit from eric

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialWeb::Application.config.secret_token = 'd320b281ce577ed67da7952d81686410675e073f8ebde476364af05ed2be29d2f6b8b521e36e9632a417b489a22a38ef85e8f86b9f733dbbed8507d14663ec7d'
